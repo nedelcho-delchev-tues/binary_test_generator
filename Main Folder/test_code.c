77d03d9d257b66193911ca75ee3cc12d0b875bb2
georgi-ivanov-tues <georgiivanof@abv.bg>
2014-06-24 18:12:21 +0300

    Making easy difficulty a bit easier, changing README

--- Main Folder/test_code.c
+++ Main Folder/test_code.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
-int value1 = 424;
-int value2 = 370;
+int value1 = 212;
+int value2 = 258;
 int result = (value1 << 3) | (value2 >> 3);
 printf("%d\n", result);
 return 0;
